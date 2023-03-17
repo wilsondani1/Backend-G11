d5426bf8ad2a82876839e08b21d2b4c6ae2798cf
wilson cutipa <wilson_dani@hotmail.com>
2023-03-17 16:40:01 -0500

    envio de correo con python

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from os import environ
 from db import conexion
 from flask_restful import Api
+
+from utils.enviar_correo import enviar_correo_adjuntos
 from controllers.usuario_controller import RegistroController
 
 load_dotenv()
@@ -15,6 +17,15 @@ conexion.init_app(app)
 api = Api(app)
 
 Migrate(app, conexion)
+
+@app.route('/prueba')
+def enviar_correo_prueba():
+    #aqui se coloca el correo del destinatario
+    enviar_correo_adjuntos('wilson_dani1@hotmail.com', 'Correo con imagenes')
+
+    return {
+        'message': 'Correo enviado exitosamente'
+    }
 
 
 api.add_resource(RegistroController, '/registro')
