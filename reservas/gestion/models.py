acabbd2ff222fbe58518bb5760374a0874bf467f
wilson cutipa <wilson_dani@hotmail.com>
2023-03-22 23:10:51 -0500

    s6_dia1

--- reservas/gestion/models.py
+++ reservas/gestion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class categorias(models.Model):
+    id = models.AutoField(primary_key=True,unique=True)
+    nombre = models.TextField(null=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        #sirve para modificar alguna configuracion de la tabla de nuestra id
+        db_table = 'categorias'
