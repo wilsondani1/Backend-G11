34d56b53d042b486fb7df4fae993367fd919be22
wilson cutipa <wilson_dani@hotmail.com>
2023-03-29 21:59:22 -0500

    creacion tablausuario

---
+++ veterinaria/gestion/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+
+class ManejoUsuario(BaseUserManager):
+    def create_superuser(self,correo,nombre,apellido,password,tipoUsuario):
+        if not correo:
+            raise ValueError('El debe tener correo')
+        
+        correo_normalizado = self.normalize_email(correo)
+
+        nuevo_usuario = self.model(correo = correo_normalizado,nombre = nombre,apellido = apellido,tipoUsuario = tipoUsuario)
+        nuevo_usuario.set_password(password)
+        nuevo_usuario.is_superuser = True
+        nuevo_usuario.is_staff = True
+        nuevo_usuario.save()
+
+class Usuario(AbstractBaseUser,PermissionsMixin):
+    id = models.AutoField(primary_key= True,unique=True)
+
+    nombre = models.TimeField(null=False)
+    apellido = models.TimeField(null=False)
+    correo = models.EmailField(max_length=100,unique=True,null=False)
+    password = models.TextField(null=False)
+    tipousuario = models.CharField(max_length=100,choices=[('ADMIN','ADMIN'),('CLIENTE','CLIENTE')],db_column='tipo_usuario')
+
+    is_staff = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'correo'
+
+    REQUIRED_FIELDS = ['nombre','apellido','tipousuario']
+
+    objectes = ManejoUsuario()
+    class Meta :
+        db_table = 'usuario'
+
+# Create your models here.
