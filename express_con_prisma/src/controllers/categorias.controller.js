9fd767aa9d9634bb4f03493a6f40637f20236678
wilson cutipa <wilson_dani@hotmail.com>
2023-04-14 23:00:37 -0500

    sem8_ dia 3

--- express_con_prisma/src/controllers/categorias.controller.js
+++ express_con_prisma/src/controllers/categorias.controller.js
@@ -20,4 +20,83 @@ export const listarCategorias = async (req,res) => {
   res.json({
     content : categorias,
   });
-}
+};
+export const devolverCategoria = async (req,res) =>{
+  const {id} = req.params;
+  const categoria = await Prisma .categoria.findFirst({
+    where:{
+      id:+id,
+    },
+  });
+  if (!categoria) {
+    res.status(400).json({
+      message:"la categoria no existe "
+    });
+  }
+
+  return res.status(200).json({
+    content : categoria,
+  });
+};
+
+export const actualizarCategoria = async (req,res) =>{
+ const data = req.body;
+ const {id} = req.params;
+
+  const categoria = await Prisma .categoria.findFirst({
+    where:{
+      id:+id,
+    },
+
+    select:{
+      id:true,
+    },
+  });
+
+  if (!categoria) {
+    res.status(400).json({
+      message:"la categoria no existe "
+    });
+  }
+
+   const categriaActualizada = await Prisma .categoria.update({
+    where:{
+      id:categoria.id,
+    },
+    data:data,
+   });
+
+   return res.json({
+    message : "categoria actualiada exitosamente",
+    content : categriaActualizada,
+   });
+  };
+
+
+  export const eliminarCategoria = async (req,res) =>{
+    const {id} = req.params;
+
+    const categoria = await Prisma .categoria.findFirst({
+      where:{
+        id:+id,
+      },
+      select:{
+        id:true,
+      },
+    });
+
+    if (!categoria) {
+      res.status(400).json({
+        message:"la categoria no existe "
+      });
+    }
+
+    const categoriaEliminada = await Prisma .categoria.delete({
+        where:{id:+id},
+        });
+        return res.json({
+          message :"categoria eliminada exitosamente",
+          content :categoriaEliminada,
+        });
+      };
+
--- express_con_prisma/src/routes/categorias.routes.js
+++ express_con_prisma/src/routes/categorias.routes.js
@@ -1,7 +1,17 @@
 
 import { Router } from "express";
 import { crearCategoria } from "../controllers/categorias.controller.js";
+import { listarCategorias } from "../controllers/categorias.controller.js";
+import { devolverCategoria } from "../controllers/categorias.controller.js";
+import { actualizarCategoria } from "../controllers/categorias.controller.js";
+import { eliminarCategoria } from "../controllers/categorias.controller.js";
 
 export const categoriaRouter = Router();
 
-categoriaRouter.post(crearCategoria);
+categoriaRouter.route('/categorias').post(crearCategoria).get(listarCategorias);
+categoriaRouter
+.route('/categorias/:id')
+.get(devolverCategoria)
+.patch(actualizarCategoria)
+.put(actualizarCategoria)
+.delete(eliminarCategoria);
